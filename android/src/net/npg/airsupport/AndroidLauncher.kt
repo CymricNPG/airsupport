6caad289dbe6b9f698573b7ae207b2f61b2574f7
Roland Spatzenegger <c@npg.net>
2017-08-23 23:28:10 +0200

    fix lib

--- android/src/net/npg/airsupport/AndroidLauncher.kt
+++ android/src/net/npg/airsupport/AndroidLauncher.kt
@@ -6,9 +6,8 @@ import com.badlogic.gdx.backends.android.AndroidApplication
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration
 
 class AndroidLauncher : AndroidApplication() {
-    override fun onCreate(savedInstanceState: Bundle) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val config = AndroidApplicationConfiguration()
-        initialize(AirSupportMain(), config)
+        initialize(AirSupportMain(), AndroidApplicationConfiguration())
     }
 }
